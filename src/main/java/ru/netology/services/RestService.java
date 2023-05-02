1bf28e982a71c57b25750752bea490fca2ef4733
Aleksandr Trufmanov <kride2009@yandex.ru>
2023-05-02 13:27:39 +0300

    done1

--- src/main/java/ru/netology/services/RestService.java
+++ src/main/java/ru/netology/services/RestService.java
@@ -1,12 +1,12 @@
 package ru.netology.services;
 
 public class RestService {
-    public int calculate(int income, int expenses, int threshold) {
-        threshold = 20_000;
+    public RestService(int income, int expenses, int threshold) {
+    }
+
+    public int countRest(int income, int expenses, int threshold) {
         int count = 0;
         int money = 0;
-        income = 10_000;
-        expenses = 3_000;
         for (int month = 1; month <= 12; month++) {
 
             if (money >= threshold) { // можем отдыхать
@@ -15,11 +15,6 @@ public class RestService {
             } else {
                 money = money + income - expenses;
             }
-            // System.out.println("Месяц " + month);
-            //System.out.println("Доход: " + income);
-            // System.out.println("Расход: " + expenses);
-            // System.out.println("Баланс: " + money);
-            //System.out.println("Кол-во месяцев для отдыха: " + count);
         }
         return count;
     }
--- src/test/java/ru/netology/services/RestServiceTest.java
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -2,20 +2,17 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Test;
 
+
 public class RestServiceTest {
 
+    int threshold = 150_000;
+    int income = 100_000;
+    int expenses = 60_000;
+
     @Test
-   void calcCount(int income, int expenses, int threshold) {
-       income = 10_000;
-       expenses = 3_000;
-       threshold = 20_000;
-    RestService service = new RestService();
-        // подготавливаем данные:
-       int count = service.calculate(income, expenses, threshold);
-     // System.out.println("Месяц " + month);
-      //System.out.println("Доход: " + income);
-    // System.out.println("Расход: " + expenses);
-     // System.out.println("Баланс: " + money);
-      System.out.println("Кол-во месяцев для отдыха: " + count);
+    void cauntRest() {
+        RestService service = new RestService(income, expenses, threshold);
+        int count = service.countRest(income, expenses, threshold);
+        System.out.println("Кол-во месяцев для отдыха: " + count);
     }
 }
